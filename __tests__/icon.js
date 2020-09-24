d38e07fd265683e0919eaea162dfe19fbdacaaf2
Pau Joan <pregonipaw@hotmail.com>
2020-09-24 19:01:03 +0200

    first commit

---
+++ __tests__/icon.js
@@ -0,0 +1,67 @@
+import { mount } from '@vue/test-utils';
+import AndroidIcon from '../dist/Android';
+
+const WrappedIcon = {
+  components: {
+    AndroidIcon,
+  },
+  render(h) {
+    return h(AndroidIcon, {
+      attrs: this.$attrs,
+      listeners: this.$listeners,
+    });
+  },
+};
+
+describe('Icon', () => {
+  let icon;
+
+  beforeEach(() => {
+    icon = mount(WrappedIcon);
+  });
+
+  it('accepts a "title" property', () => {
+    expect(icon.attributes()['aria-label']).toEqual('Android icon');
+
+    icon.setProps({ title: 'foo' });
+
+    expect(icon.attributes()['aria-label']).toEqual('foo');
+  });
+
+  it('accepts a "decorative" property', () => {
+    expect(icon.attributes()['aria-hidden']).toBeFalsy();
+
+    icon.setProps({ decorative: true });
+
+    expect(icon.attributes()['aria-hidden']).toBeTruthy();
+  });
+
+  it('accepts a "fillColor" property', () => {
+    const svg = icon.find('.material-design-icon__svg');
+
+    expect(svg.attributes()['fill']).toEqual('currentColor');
+
+    icon.setProps({ fillColor: '#FF0000' });
+
+    expect(svg.attributes()['fill']).toEqual('#FF0000');
+  });
+
+  it('listens to a click event', () => {
+    const clickListener = jest.fn();
+    const iconWithEvent = mount({
+      name: 'IconWithEvent',
+      components: { AndroidIcon },
+      template: `
+        <AndroidIcon
+          @click="clickListener"
+        />
+      `,
+      methods: {
+        clickListener,
+      },
+    });
+
+    iconWithEvent.trigger('click');
+    expect(clickListener).toBeCalled();
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: '<rootDir>/reports/jest' }],
+  ],
+};
